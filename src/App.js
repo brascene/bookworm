2c911f8cf2ec566c0d876da0ced2b2633582473b
brascene <bra.scene@gmail.com>
2017-08-31 13:54:18 +0200

    router basic setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
+import HomePage from './components/HomePage'
+import Login from './components/Login'
 
-const App = () => <div>Hello world</div>
+const App = () => <div>
+  <Route path="/" exact component={HomePage} />
+  <Route path="/login" exact component={Login} />
+</div>
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Login = () => (
+  <div>
+    <h1>Login Page</h1>
+    <Link to="/">Back to home</Link>
+  </div>
+)
+
+export default Login
