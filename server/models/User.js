978a0268d1c0f6019832d36e90713cc2a7576973
brascene <bra.scene@gmail.com>
2017-09-17 16:04:14 +0200

    user sign up

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import uniqueValidator from 'mongoose-unique-validator'
 
 // to do add uniqueness and email validationsto email field
 const schema = new mongoose.Schema({
-  email: { type: String, required: true, lowercase: true, index: true },
-  passwordHash: { type: String, required: true }
+  email: { type: String, required: true, lowercase: true, index: true, unique: true },
+  passwordHash: { type: String, required: true },
+  confirmed: { type: Boolean, default: false}
 }, { timestamps: true})
 
 schema.methods.isValidPassword = function isValidPassword(password) {
@@ -21,8 +23,15 @@ schema.methods.generateJWT = function generateJWT() {
 schema.methods.toAuthJSON = function toAuthJSON() {
   return {
     email: this.email,
+    confirmed: this.confirmed,
     token: this.generateJWT()
   }
 }
+
+schema.methods.setPassword = function setPassword(password) {
+  this.passwordHash = bcrypt.hashSync(password, 10)
+}
+
+schema.plugin(uniqueValidator, { message: 'This email is already reserved'})
 
 export default mongoose.model('User', schema)
--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Login from './components/pages/Login'
 import Dashboard from './components/pages/Dashboard'
 import UserRoute from './components/routes/UserRoute'
 import GuestRoute from './components/routes/GuestRoute'
+import SignupPage from './components/pages/SignupPage'
 import PropTypes from 'prop-types'
 
 const App = ({ location }) => <div className="ui container">
   <Route location={location} path="/" exact component={HomePage} />
   <GuestRoute location={location} path="/login" exact component={Login} />
+  <GuestRoute location={location} path="/signup" exact component={SignupPage} />
   <UserRoute location={location} path="/dashboard" exact component={Dashboard} />
 </div>
 
